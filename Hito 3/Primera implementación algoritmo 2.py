59d9394d4afc854be95bfb498debbf5fbc166c6b
ShoLee01 <67173454+ShoLee01@users.noreply.github.com>
2021-11-27 14:28:32 -0500

    close #28

---
+++ Hito 3/Primera implementación algoritmo 2.py
@@ -0,0 +1,19 @@
+#Miguel Angel Alfaro Gonzales U201910015
+
+def prim(G):
+  n = len(G)
+  visited = [False]*n
+  path = [-1]*n
+  cost = [math.inf]*n
+  q = [(0, 0)]
+  while q:
+    _, u = hq.heappop(q)
+    if not visited[u]:
+      visited[u] = True
+      for v, w in G[u]:
+        if not visited[v] and w < cost[v]:
+          cost[v] = w
+          path[v] = u
+          hq.heappush(q, (w, v))
+
+  return path, cost
